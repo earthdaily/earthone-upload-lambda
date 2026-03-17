5318175979a7d6f820c586e8303a858fc9af3443
Katrina Jelinek <katrina@geosite.io>
2026-03-17 11:33:15 -0600

    Initial commit - EarthOne upload lambda

---
+++ handler.py
@@ -0,0 +1,39 @@
+import json
+import os
+import boto3
+
+_sm = boto3.client("secretsmanager")
+s3 = boto3.client("s3")
+
+
+def lambda_handler(event, context):
+    secret_id = os.environ["EARTHONE_SECRET_ID"]
+    secrets = _sm.get_secret_value(SecretId=secret_id)
+    secrets_json = json.loads(secrets["SecretString"])
+
+    os.environ["EARTHONE_CLIENT_ID"] = secrets_json["client_id"]
+    os.environ["EARTHONE_CLIENT_SECRET"] = secrets_json["client_secret"]
+    import earthdaily.earthone as eo
+
+    tiff_key = event["tiff_key"]
+    bucket = event["bucket"]
+    metadata = event["metadata"]
+
+    head = s3.head_object(Bucket=event["bucket"], Key=tiff_key)
+    if head["ContentLength"] <= 0:
+        raise ValueError(f"Image file is empty: {tiff_key}")
+    
+    local_path = f"/tmp/{os.path.basename(tiff_key)}"
+    s3.download_file(bucket, tiff_key, local_path)
+
+    product = eo.catalog.Product.get("earthdaily:maxar-gegd-rgb:v1")
+
+    image = eo.catalog.Image(product=product, name=metadata["id"], geometry=metadata["geometry"], acquired=metadata["start_date"], acquired_end=metadata["stop_date"])
+    upload = image.upload(local_path)
+    upload.wait_for_completion()
+
+    metadata["aoi"] = upload.image.id
+
+    return {
+        "metadata": metadata
+    }
